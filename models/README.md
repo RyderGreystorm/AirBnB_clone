074a3bebe8986281f8cc99245b171d61417d845e
Godbless Biekro <biekrogodbless@gmail.com>
2023-07-12 00:26:46 +0000

    first commit

---
+++ models/README.md
@@ -0,0 +1,2 @@
+#AirBnB Clone
+This project is the complete backend of the airbnb platform with a cli as frontend
---
+++ models/base_model.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+"""
+Base class for our project.
+Ths Base class contains most of the information and logic other
+classes would need
+"""
+
+import uuid
+from datetime import datetime
+
+class BaseModel:
+    """
+    This is the main model from which other classes will
+    inherit from
+    """
+
+    def __init__(self):
+        """
+        Each time the class is instantaited, it will
+        have all the attributes listed below
+        """
+
+        self.id = uuid.uuid4()
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+
+    def get_id(self):
+        """
+        getter to get the id of the model created
+        """
+        return str(self.id)
+
+    def get_created_at(self):
+        """
+        method that returns the date at which the object was
+        created
+        """
+
+        return self.created_at.isoformat()
+
+    def get_updated_at(self):
+        """
+        getter to get the date the item was last updated
+        """
+
+        return self.updated_at.isoformat()
+
+    def __str__(self):
+        """
+        returns the string representation of the object
+        """
+
+        return f"[{type(self).__name__}] ({self.get_id}) {self.__dict__}"
+
+    def save(self):
+        """
+        saves the updates made to the instance of our baseModel  class
+        """
+
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """
+        returns the dictionary representation of an object
+        """
+        self.__dict__["__class__"] = type(self).__name__
+        self.__dict__["created_at"] = self.get_created_at()
+        self.__dict__["updated_at"] = self.get_updated_at()
+        return self.__dict__
