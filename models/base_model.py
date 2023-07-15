fa6b47c66fcb2f32a46bb34d9adc6be024b268fa
Stepha-Nek <Nneka.oguh1@gmail.com>
2023-07-15 09:11:34 -0700

    check

--- models/base_model.py
+++ models/base_model.py
@@ -1,79 +1,49 @@
-#!/usr/bin/env python3
-"""
-Base class for our project.
-Ths Base class contains most of the information and logic other
-classes would need
-"""
-
-import uuid
+#!/usr/bin/python3
+"""Defines the BaseModel class."""
+import models
+from uuid import uuid4
 from datetime import datetime
 
+
 class BaseModel:
-    """
-    This is the main model from which other classes will
-    inherit from
-    """
+    """Represents the BaseModel of the HBnB project."""
 
     def __init__(self, *args, **kwargs):
+        """Initialize a new BaseModel.
+        Args:
+            *args (any): Unused.
+            **kwargs (dict): Key/value pairs of attributes.
         """
-        Each time the class is instantaited, it will
-        have all the attributes listed below
-        """
-
-        if kwargs:
-            """removing the __class__"""
-            kwargs.pop("__class__", None)
-            for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    """ Converting back to object"""
-                    setattr(self, key,datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+        tform = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+        if len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    self.__dict__[k] = datetime.strptime(v, tform)
                 else:
-                    setattr(self, key, value)
+                    self.__dict__[k] = v
         else:
-            self.id = uuid.uuid4()
-            self.created_at = datetime.now()
-            self.updated_at = self.created_at
-
-    def get_id(self):
-        """
-        getter to get the id of the model created
-        """
-        return str(self.id)
-
-    def get_created_at(self):
-        """
-        method that returns the date at which the object was
-        created
-        """
-
-        return self.created_at.isoformat()
-
-    def get_updated_at(self):
-        """
-        getter to get the date the item was last updated
-        """
-
-        return self.updated_at.isoformat()
-
-    def __str__(self):
-        """
-        returns the string representation of the object
-        """
-
-        return f"[{type(self).__name__}] ({self.get_id}) {self.__dict__}"
+            models.storage.new(self)
 
     def save(self):
-        """
-        saves the updates made to the instance of our baseModel  class
-        """
-
-        self.updated_at = datetime.now()
+        """Update updated_at with the current datetime."""
+        self.updated_at = datetime.today()
+        models.storage.save()
 
     def to_dict(self):
+        """Return the dictionary of the BaseModel instance.
+        Includes the key/value pair __class__ representing
+        the class name of the object.
         """
-        returns the dictionary representation of an object
-        """
-        self.__dict__["__class__"] = type(self).__name__
-        self.__dict__["created_at"] = self.get_created_at()
-        self.__dict__["updated_at"] = self.get_updated_at()
-        return self.__dict__
+        rdict = self.__dict__.copy()
+        rdict["created_at"] = self.created_at.isoformat()
+        rdict["updated_at"] = self.updated_at.isoformat()
+        rdict["__class__"] = self.__class__.__name__
+        return rdict
+
+    def __str__(self):
+        """Return the print/str representation of the BaseModel instance."""
+        clname = self.__class__.__name__
+        return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,43 +1,53 @@
 #!/usr/bin/python3
-"""Defines the FileStorage class."""
 import json
-from models.base_model import BaseModel
-
-
+"""
+used for serializing and deserializing
+objects to and from JSON format
+"""
 
 class FileStorage:
-    """Represent an abstracted storage engine.
-    Attributes:
-        __file_path (str): The name of the file to save objects to.
-        __objects (dict): A dictionary of instantiated objects.
     """
-    __file_path = "file.json"
+    filepath is a private class attribute showing path to
+    json where objects will be stored.
+    objects is a private class sttribure empty dictionary
+    to store objects by class name and id.
+    """
+    __file_path = "file.json" 
     __objects = {}
 
     def all(self):
-        """Return the dictionary __objects."""
-        return FileStorage.__objects
+        """
+        dictionary, which contains all the objects
+        stored in the FileStorage.
+        """
+        return self.__objects
 
     def new(self, obj):
-        """Set in __objects obj with key <obj_class_name>.id"""
-        ocname = obj.__class__.__name__
-        FileStorage.__objects["{}.{}".format(ocname, obj.id)] = obj
+        """
+        adds new object  to objects dictionary.
+        takes obj as argument. class name and id
+        used as  key in dictionary to store object
+        """
+        key = f"{obj.__class__.__name__}.{obj.id}"
+        self.__objects[key] = obj
 
     def save(self):
-        """Serialize __objects to the JSON file __file_path."""
-        odict = FileStorage.__objects
-        objdict = {obj: odict[obj].to_dict() for obj in odict.keys()}
-        with open(FileStorage.__file_path, "w") as f:
-            json.dump(objdict, f)
+        data = {}
+        for key, obj in self.__objects.items():
+            data[key] = obj.to_dict()
+        with open(self.__file_path, "w") as file:
+            json.dump(data, file)
 
     def reload(self):
-        """Deserialize the JSON file __file_path to __objects, if it exists."""
         try:
-            with open(FileStorage.__file_path) as f:
-                objdict = json.load(f)
-                for o in objdict.values():
-                    cls_name = o["__class__"]
-                    del o["__class__"]
-                    self.new(eval(cls_name)(**o))
+            with open(self.__file_path, "r") as file:
+                data = json.load(file)
+                for key, obj_dict in data.items():
+                    class_name, obj_id = key.split(".")
+                    module = __import__("models." + class_name, fromlist=[class_name])
+                    cls = getattr(module, class_name)
+                    obj = cls(**obj_dict)
+                    self.__objects[key] = obj
         except FileNotFoundError:
-            return
+            pass
+
